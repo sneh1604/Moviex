946b3505b9ac789041e90523123eea5b8bac3b15
sneh1604 <22it140@charusat.edu.com>
2024-04-02 17:47:37 +0530

    updated

---
+++ frontend/src/pages/Admin/CreateMovie.jsx
@@ -0,0 +1,234 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  useCreateMovieMutation,
+  useUploadImageMutation,
+} from "../../redux/api/movies";
+import { useFetchGenresQuery } from "../../redux/api/genre";
+import { UPLOAD_URL } from "../../redux/constants"; 
+import { toast } from "react-toastify";
+const CreateMovie = () => {
+  const navigate = useNavigate();
+
+
+  const [movieData, setMovieData] = useState({
+    name: "",
+    year: 0,
+    detail: "",
+    cast: [],
+    rating: 0,
+    image: null,
+    genre: "",
+  });
+
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const [
+    createMovie,
+    { isLoading: isCreatingMovie, error: createMovieErrorDetail },
+  ] = useCreateMovieMutation();
+
+  const [
+    uploadImage,
+    { isLoading: isUploadingImage, error: uploadImageErrorDetails },
+  ] = useUploadImageMutation();
+
+  const { data: genres, isLoading: isLoadingGenres } = useFetchGenresQuery();
+
+  useEffect(() => {
+    if (genres) {
+      setMovieData((prevData) => ({
+        ...prevData,
+        genre: genres[0]?._id || "",
+      }));
+      console.log(genres[0]?._id);
+    }
+  }, [genres]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    if (name === "genre") {
+      const selectedGenre = genres.find((genre) => genre.name === value);
+
+      setMovieData((prevData) => ({
+        ...prevData,
+        genre: selectedGenre ? selectedGenre._id : "",
+      }));
+    } else {
+      setMovieData((prevData) => ({
+        ...prevData,
+        [name]: value,
+      }));
+    }
+  };
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    setSelectedImage(file);
+  };
+
+  const handleCreateMovie = async () => {
+    try {
+      if (!movieData.name || !movieData.year || !movieData.detail || !selectedImage) {
+        toast.error("Please fill all required fields");
+        return;
+      }
+
+      let uploadedImagePath = null;
+
+      if (selectedImage) {
+        const formData = new FormData();
+        formData.append("image", selectedImage);
+
+        const uploadImageResponse = await uploadImage(formData);
+
+        if (uploadImageResponse.data && uploadImageResponse.data.image) {
+          uploadedImagePath = uploadImageResponse.data.image;
+        } else {
+          console.error("Failed to upload image: ", uploadImageErrorDetails);
+          toast.error("Failed to upload image");
+          return;
+        }
+      }
+      // Create movie with uploaded image path
+      await createMovie({
+        ...movieData,
+        image: uploadedImagePath,
+      });
+
+      navigate("/admin/movies-list");
+
+      // Reset movie data state
+      setMovieData({
+        name: "",
+        year: 0,
+        detail: "",
+        cast: [],
+        rating: 0,
+        image: null,
+        genre: "",
+      });
+
+      toast.success("Movie Added To Database");
+    } catch (error) {
+      console.error("Failed to create movie: ", error);
+      toast.error("Failed to create movie");
+    }
+  };
+
+  return (
+    <div className="container flex justify-center items-center mt-4">
+      <form>
+        <p className="text-green-200 w-[50rem] text-2xl mb-4">Create Movie</p>
+        <div className="mb-4">
+          <label className="block">
+            Name:
+            <input
+              type="text"
+              name="name"
+              value={movieData.name}
+              onChange={handleChange}
+              className="border px-2 py-1 w-full"
+            />
+          </label>
+        </div>
+        <div className="mb-4">
+          <label className="block">
+            Year:
+            <input
+              type="number"
+              name="year"
+              value={movieData.year}
+              onChange={handleChange}
+              className="border px-2 py-1 w-full"
+            />
+          </label>
+        </div>
+        <div className="mb-4">
+          <label className="block">
+            Detail:
+            <textarea
+              name="detail"
+              value={movieData.detail}
+              onChange={handleChange}
+              className="border px-2 py-1 w-full"
+            ></textarea>
+          </label>
+        </div>
+        <div className="mb-4">
+          <label className="block">
+            Cast (comma-separated):
+            <input
+              type="text"
+              name="cast"
+              value={movieData.cast.join(", ")}
+              onChange={(e) =>
+                setMovieData({ ...movieData, cast: e.target.value.split(", ") })
+              }
+              className="border px-2 py-1 w-full"
+            />
+          </label>
+        </div>
+        <div className="mb-4">
+          <label className="block">
+            Genre:
+            <select
+              name="genre"
+              value={movieData.genre}
+              onChange={handleChange}
+              className="border px-2 py-1 w-full"
+            >
+              {isLoadingGenres ? (
+                <option>Loading genres...</option>
+              ) : (
+                genres.map((genre) => (
+                  <option key={genre._id} value={genre._id}>
+                    {genre.name}
+                  </option>
+                ))
+              )}
+            </select>
+          </label>
+        </div>
+
+        <div className="mb-4">
+          <label
+            style={
+              !selectedImage
+                ? {
+                    border: "1px solid #888",
+                    borderRadius: "5px",
+                    padding: "8px",
+                  }
+                : {
+                    border: "0",
+                    borderRadius: "0",
+                    padding: "0",
+                  }
+            }
+          >
+            {!selectedImage && "Upload Image"}
+            <input
+              type="file"
+              accept="image/*"
+              onChange={handleImageChange}
+              style={{ display: !selectedImage ? "none" : "block" }}
+            />
+          </label>
+        </div>
+
+        <button
+          type="button"
+          onClick={handleCreateMovie}
+          className="bg-teal-500 text-white px-4 py-2 rounded"
+          disabled={isCreatingMovie || isUploadingImage}
+        >
+          {isCreatingMovie || isUploadingImage ? "Creating..." : "Create Movie"}
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default CreateMovie;
