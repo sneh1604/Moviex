660e0e55d09de482a92c48bfb77babb5b36eb555
sneh1604 <22it140@charusat.edu.com>
2024-04-05 11:48:51 +0530

    updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": "http://localhost:3000",
-      "/uploads/": "http://localhost:3000",
+      "/api/": "https://moviex-sepia.vercel.app",
+      "/uploads/": "https://moviex-sepia.vercel.app",
     },
   },
 });
